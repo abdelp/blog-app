5486acdc212aaca7cbd865c02dd0da9a19f1edda
abdelp <juniorperezpy@gmail.com>
2020-04-14 12:47:09 -0400

    Add initial comments controller structure

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
     @comment = Comment.new
     @comment.article_id = @article.id
-
-    @article = Article.find(params[:id])
   end
 
   def new
